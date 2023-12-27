c924b6b48b1dc57ef513a1574fab05ea7a807f48
cejiogu <chimdije@gmail.com>
2023-12-27 11:39:43 -0500

    Implemented Many-to-Many relationship between Restaurants and Users

--- backend/db.py
+++ backend/db.py
@@ -2,6 +2,12 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+# Assocation table relating Restaurant table to User table
+rest_user_association = db.Table("rest_user_association", db.Model.metadata,
+                                  db.Column("rest_id", db.Integer, db.ForeignKey("restaurants.id")),
+                                  db.Column("user_id", db.Integer, db.ForeignKey("users.id")))
+
+
 class Restaurant(db.Model):
     """
     Restaurant Model
@@ -10,6 +16,8 @@ class Restaurant(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True) # Do we need to include "nullable=False" in this line?
     name = db.Column(db.String, nullable=False)
     desc = db.Column(db.String, nullable=False) # TODO: Limit character size of description
+
+    users = db.relationship("User", secondary=rest_user_association, back_populates="users")
 
     def __init__(self, **kwargs):
         """
@@ -26,6 +34,8 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     identifier = db.Column(db.String, nullable=False)
     password = db.Column(db.String, nullable=False)
+
+    restaurants = db.relationship("Restaurant", secondary=rest_user_association, back_populates="users")
 
     def __init__(self, **kwargs):
         """
