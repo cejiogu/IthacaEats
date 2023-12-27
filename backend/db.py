279fb5ee409240944e6a918dccacf29a99b838ae
cejiogu <chimdije@gmail.com>
2023-12-27 11:49:42 -0500

    Implement serialize functions for User and Restaurant classes

--- backend/db.py
+++ backend/db.py
@@ -26,6 +26,17 @@ class Restaurant(db.Model):
         self.name = kwargs.get("name")
         self.desc = kwargs.get("desc")
 
+    def serialize(self):
+        """
+        Serialize a Restaurant Object
+        """
+        return {
+            "id": self.id,
+            "name": self.name,
+            "desc": self.desc,
+            "users": [u.simple_serialize() for u in self.users]
+        }
+
 class User(db.Model):
     """
     User Model
@@ -41,5 +52,29 @@ class User(db.Model):
         """
         Initialize a User Object
         """
-        self.identifier = kwargs.get("identifier")
+        self.f_name = kwargs.get("first_name")
+        self.l_name = kwargs.get("last_name")
+        self.username = kwargs.get("username")
+        self.email = kwargs.get("email")
         self.password = kwargs.get("password")
+
+    def serialize(self):
+        """
+        Serialize a User Object
+        """
+        return {
+            "id": self.id,
+            "first_name": self.f_name,
+            "last_name": self.l_name,
+            "username": self.username,
+            "email": self.email
+        }
+
+    def simple_serialize(self):
+        """
+        Serialize a User Object, providing less information for security purposes
+        """
+        return {
+            "id": self.id,
+            "username": self.username
+        }
