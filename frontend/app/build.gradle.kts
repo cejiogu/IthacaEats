2651bd4602914b88378828457dbc84a93cb22cc3
cejiogu <chimdije@gmail.com>
2024-01-18 22:54:59 -0500

    Develop boilerplate CardView to represent restaurants

--- frontend/app/build.gradle.kts
+++ frontend/app/build.gradle.kts
@@ -74,4 +74,7 @@ dependencies {
     // Dependencies for ConstraintLayout
     implementation("androidx.constraintlayout:constraintlayout:2.2.0-alpha13")
     implementation("androidx.constraintlayout:constraintlayout-compose:1.1.0-alpha13")
+
+    // Dependency for CardView
+    implementation("androidx.cardview:cardview:1.0.0")
 }
