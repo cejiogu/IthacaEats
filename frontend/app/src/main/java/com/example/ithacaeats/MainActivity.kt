21fbc9eef6c8d5545f1df681f0098bec2efeaf26
cejiogu <chimdije@gmail.com>
2024-01-18 22:55:55 -0500

    Establish recyclerview layout manager

--- frontend/app/src/main/java/com/example/ithacaeats/MainActivity.kt
+++ frontend/app/src/main/java/com/example/ithacaeats/MainActivity.kt
@@ -10,13 +10,17 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.ithacaeats.ui.theme.IthacaEatsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
+        setContentView(R.layout.activity_main)
 
-        }
+
+        val recyclerView : RecyclerView = findViewById(R.id.recycler)
+        recyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
